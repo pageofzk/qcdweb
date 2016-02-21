8fd0e8b859c161a553de709a595f127534503c87
王大利 <zengkaizy@163.com>
2016-02-21 18:32:33 +0800

    fix index

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 app.use('/content', content);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'QCD' });
+  res.render('index', { title: '器材党' , message: '即将隆重上线！'});
 });
 
 module.exports = router;
