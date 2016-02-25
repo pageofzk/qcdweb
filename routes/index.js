d17af26dd71988be84dcf026fbd4761a923ede23
pageofzk <zengkaizy@163.com>
2016-02-25 21:47:46 +0800

    add web index

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,56 @@ var express = require('express');
 var router = express.Router();
 //leancloud init
 var AV = require('leanengine');
-
+var pageNum = 12;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    var obj = {}, ID = req.params.id;
-    console.log(ID);
-    var query = new AV.Query('Lib');
-    query.get(ID).then(function(post) {
-        obj = post.toJSON();
-        obj.title = obj.brand + " " + obj.top_type + " " + obj.name;
-        console.log(obj);
+router.get('/p:page', function(req, res, next) {
+    var obj = {}, page = req.params.page;
+    if (!page || parseInt(page) <= 0) page = 1;
+    var query = new AV.Query('Post');
+    query.notEqualTo('doc_type', 'test');
+    query.skip(pageNum*(page-1));
+    query.limit(pageNum);
+    query.addDescending("time");
+    query.find().then(function(results) {
+        obj.title =  '器材党';
+        obj.page = parseInt(page);
+        obj.items = [];
+        // 处理返回的结果数据
+        for (var i = 0; i < results.length; i++) {
+            var result = results[i].toJSON();
+            // 标红内容
+            if (!result.price_txt)
+                result.price_txt = '资讯';
+            obj.items[i]=result;
+        }
         return res.render('index', obj);
     }, function(error) {
-        res.status(error.status || 500);
-        res.render('error', {
-            code: error.code,
-            message: error.message,
-            error: {}
-        });
+        console.log('Error: ' + error.code + ' ' + error.message);
     });
-    res.render('index', { title: '器材党' , message: '即将隆重上线！'});
+});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var obj = {};
+    var query = new AV.Query('Post');
+    query.notEqualTo('doc_type', 'test');
+    query.limit(pageNum);
+    query.addDescending("time");
+    query.find().then(function(results) {
+        obj.title =  '器材党';
+        obj.page = 1;
+        obj.items = [];
+        // 处理返回的结果数据
+        for (var i = 0; i < results.length; i++) {
+            var result = results[i].toJSON();
+            // 标红内容
+            if (!result.price_txt)
+                result.price_txt = '资讯';
+            obj.items[i]=result;
+        }
+        return res.render('index', obj);
+    }, function(error) {
+        console.log('Error: ' + error.code + ' ' + error.message);
+    });
 });
 
 module.exports = router;
